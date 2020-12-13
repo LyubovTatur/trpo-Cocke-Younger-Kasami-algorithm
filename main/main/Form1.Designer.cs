c9fc3982d5e8fd650758dd6d3a41d9778a957f3c
LyubovTatur <lubatatur58@gmail.com>
2020-12-13 19:48:45 +0300

    done!

---
+++ main/main/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace main
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 71);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(184, 334);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "S -> AB\nA -> CD | CF\nB -> c | EB\nC -> a\nD -> b\nE -> c\nF -> AD\nL -> SB\nK -> CC";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Grammar:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "aaabbbcc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Word:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(220, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(556, 367);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(220, 16);
+            this.trackBar1.Maximum = 0;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(556, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ main/main/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace main
+{
+    public partial class Form1 : Form
+    {
+        CYK cyk = new CYK();
+        // Regex reg = new Regex(@"(\w+)\s+->\s+(\w+)\|*(\w+)*");
+        Regex reg = new Regex("(\\w+)\\s*->\\s*(\\w+)\\s*\\|*\\s*(\\w+)*");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 1;
+            dataGridView1.ColumnCount = 1;
+            dataGridView1.Rows.Clear();
+            cyk.Clear();
+            cyk.word = textBox1.Text;
+            foreach (var line in richTextBox1.Lines)
+            {
+                if (reg.IsMatch(line))
+                {
+                    cyk.AddNonterm(reg.Match(line).Groups[1].Value, new List<string> { reg.Match(line).Groups[2].Value, reg.Match(line).Groups[3].Value, reg.Match(line).Groups[4].Value, reg.Match(line).Groups[5].Value });
+                    // MessageBox.Show(reg.Match(line).Groups[1].Value + " " + reg.Match(line).Groups[2].Value + " " + reg.Match(line).Groups[3].Value + " " + reg.Match(line).Groups[4].Value + " " + reg.Match(line).Groups[5].Value + "\\\\ " + line);
+                }
+            }
+          
+
+            dataGridView1.ColumnCount = cyk.word.Length;
+            dataGridView1.RowCount = cyk.word.Length;
+            
+
+            int J = dataGridView1.RowCount - 1;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = dataGridView1.RowCount - 1; j > J; j--)
+                {
+
+                    dataGridView1[j, i].Style.BackColor = Color.Gray;
+                }
+                J--;
+            }
+            //for (int j = 0; j > dataGridView1.RowCount; j++)
+            //    dataGridView1.Cells[j,0].va = cyk.word[j].ToString();
+            trackBar1.Maximum = cyk.word.Length;
+            var table = cyk.algorythm2try();
+            int n = dataGridView1.RowCount;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < table[i][j].Count; k++)
+                    {
+                        dataGridView1[j, i].Value+= table[i][j][k];
+                    }
+                    
+                }
+                n--;
+            }
+
+        }
+    }
+
+    public class CYK 
+    {
+        public string word;
+
+        List<Nonterminal> nonterminals = new List<Nonterminal>();
+        public void AddNonterm(string nonterm, List<string> terms)
+        {
+            nonterminals.Add(new Nonterminal(nonterm, terms));
+        }
+
+        public void Clear()
+        {
+            nonterminals = new List<Nonterminal>();
+        }
+        
+        public void algorithm1try()
+        {
+            int n = word.Length, r = nonterminals.Count;
+            bool[,,] P = new bool[n,n,r];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int t = 0; t < r; t++)
+                    {
+                        P[i, j, t] = false;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < r; j++)
+                {
+                    if (nonterminals[j].terminals.Contains(word[i].ToString()))
+                    {
+                        P[1, i, j] = true;
+                    }
+                }
+            }
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < n-i+1; j++)
+                {
+                    for (int k = 0;  k < i-1;  k++)
+                    {
+
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (P[n-1,1,i])
+                {
+                    MessageBox.Show("d");
+                }
+                
+            }
+
+        }
+        
+        public List<List<List<string>>> algorythm2try()
+        {
+            int n = word.Length, r = nonterminals.Count;
+            List<List<List<string>>> table = new List<List<List<string>>>();
+            for (int i = 0; i < n; i++)
+            {
+                table.Add(new List<List<string>>());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    table[i].Add(new List<string>());
+                }
+                    
+                
+            }
+            for (int i = 0; i < n; i++)
+            {
+                foreach (var nonterm in nonterminals)
+                {
+                    foreach (var term in nonterm.terminals)
+                    {
+                        if (word[i].ToString() == term)
+                        {
+                            
+                                table[0][i].Add( nonterm.nonterminal);
+                            
+                        }
+                    }
+                }
+            }
+            int n1 = n-1;
+            
+            for (int i = 1; i < n; i++) //иду по стпрочкам
+            {
+                for (int j = 0; j < n1; j++)// столбцы
+                { 
+                    int stolbec1 = j, stolbec2 = j+1;//начальные
+                    int w = i - 1;
+                   // MessageBox.Show(w.ToString());
+                    // мы должны брать построчно что то
+                    for (int k = 0; k < i; k++)//строчки от нуля до позиции
+                    {
+                        //MessageBox.Show(table[0][0].Count.ToString() + " " + table[0][1].Count.ToString());
+                        // MessageBox.Show(w.ToString());
+                        //MessageBox.Show(table[k][stolbec1].Count.ToString() + " " + table[w][stolbec2].Count.ToString());
+                        if (table[k][stolbec1].Count>0  && table[w][stolbec2].Count > 0)
+                        {
+                            
+
+                            List<string>znach1 = table[k][stolbec1];
+                                List<string>znach2 = table[w][stolbec2];
+                            
+                                
+
+
+                           
+                            List<string> rescombines = new List<string>();
+                            for (int combs1 = 0; combs1 < znach1.Count; combs1++)
+                            {
+                                for (int combs2 = 0; combs2 < znach2.Count; combs2++)
+                                {
+                                    rescombines.Add(znach1[combs1] + znach2[combs2]);
+                                    
+                                }
+                            }
+                            foreach (var non in nonterminals)
+                            {
+
+                                foreach (var term in non.terminals)
+                                {
+                                    foreach (var combs in rescombines)
+                                    {
+                                        if (term == combs)
+                                        {
+
+                                            if (table[i][j].Count >0)
+                                            {
+                                                
+                                                
+                                                
+                                                bool g = false;
+                                                for (int h = 0; h < table[i][j].Count; h++)
+                                                {
+                                                    if (table[i][j][h] == non.nonterminal)
+                                                    {
+                                                        g = true;
+                                                    }
+                                                }
+                                                if (!g)
+                                                {
+                                                    table[i][j].Add(non.nonterminal);
+                                                }
+                                                
+                                            }
+                                            else
+                                            {
+                                                table[i][j].Add(non.nonterminal);
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        stolbec2++;
+                        w--;
+                        
+                    }
+                }
+                n1--;//дальше они уменьшгаются
+            }
+            return table;
+        }
+       
+    }
+
+    public class Nonterminal
+    {
+        public string nonterminal;
+        public List<string> terminals = new List<string>();
+
+        public Nonterminal(string nonterminal, List<string> terminals)
+        {
+            this.nonterminal = nonterminal;
+            this.terminals = terminals;
+        }
+    }
+}
